36027ccbcf3737918ad05f1bd6e8f32890fe22aa
Jessé Willian Picão <58005176+willianpicao@users.noreply.github.com>
2024-11-02 15:08:22 -0300

    Update main.py

--- controller/app/main.py
+++ controller/app/main.py
@@ -1,23 +1,5 @@
-"""
-PALANTIR Project (PM-MT/SESP, Fapemat and UFMT)
-
-Application Controller.
-
-__auth__="Maxweel Carmo"
-__date__="dd/04/2021"
-ver: 0.1
-"""
-
 from flask_cors import CORS
 import connexion
-
-# GEOTIFF_DIR = 'assets/geotiff'
-
-#def set_cors_headers_on_response(response):
-#    response.headers['Access-Control-Allow-Origin'] = 'http://0.0.0.0:8000'
-#    response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With'
-##    response.headers['Access-Control-Allow-Methods'] = 'OPTIONS'
-##    return response
 
 #Create the application instance
 app = connexion.App(__name__, specification_dir='./api')
